efb24658a09ee9477b14508237486dee84e36dde
zhujiang <ijensenzhu@gmail.com>
2018-06-12 01:40:23 +0800

    咨询页

--- app/src/main/java/com/dayishiapp/doctor/ui/consult/ConsultFragment.java
+++ app/src/main/java/com/dayishiapp/doctor/ui/consult/ConsultFragment.java
@@ -1,21 +1,124 @@
 package com.dayishiapp.doctor.ui.consult;
 
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
-
+import android.view.ViewGroup;
+import android.widget.TextView;
 import com.dayishiapp.doctor.R;
 import com.dayishiapp.doctor.ui.MvpFragment;
-
+import com.qmuiteam.qmui.widget.QMUITabSegment;
+import java.util.HashMap;
+import java.util.Map;
 import javax.inject.Inject;
+import butterknife.BindView;
 
 public class ConsultFragment extends MvpFragment<ConsultView,ConsultPresenter> implements ConsultView{
 
     @Inject
     ConsultPresenter consultPresenter;
+
+    @BindView(R.id.tabSegment)
+    QMUITabSegment mTabSegment;
+    @BindView(R.id.contentViewPager)
+    ViewPager mContentViewPager;
+
+    private Map<ContentPage, View> mPageMap = new HashMap<>();
+    private ContentPage mDestPage = ContentPage.Item1;
+
+    private PagerAdapter mPagerAdapter = new PagerAdapter() {
+        @Override
+        public boolean isViewFromObject(View view, Object object) {
+            return view == object;
+        }
+
+        @Override
+        public int getCount() {
+            return ContentPage.SIZE;
+        }
+
+        @Override
+        public Object instantiateItem(final ViewGroup container, int position) {
+            ContentPage page = ContentPage.getPage(position);
+            View view = getPageView(page);
+            ViewGroup.LayoutParams params = new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT);
+            container.addView(view, params);
+            return view;
+        }
+
+        @Override
+        public void destroyItem(ViewGroup container, int position, Object object) {
+            container.removeView((View) object);
+        }
+
+    };
+
+    private View getPageView(ContentPage page) {
+        View view = mPageMap.get(page);
+        if (view == null) {
+            TextView textView = new TextView(getContext());
+            textView.setGravity(Gravity.CENTER);
+            textView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 20);
+            textView.setTextColor(ContextCompat.getColor(getContext(), R.color.app_color_description));
+
+            if (page == ContentPage.Item1) {
+                textView.setText("电话");
+            } else if (page == ContentPage.Item2) {
+                textView.setText("门诊");
+            }
+
+            view = textView;
+            mPageMap.put(page, view);
+        }
+        return view;
+    }
+
     @Override
     protected void initViews(View view) {
         toolbar.setTitle(R.string.consult);
+        initTabAndPager();
     }
+
+    private void initTabAndPager() {
+        mContentViewPager.setAdapter(mPagerAdapter);
+        mContentViewPager.setCurrentItem(mDestPage.getPosition(), false);
+        mTabSegment.setHasIndicator(true);
+        mTabSegment.setIndicatorPosition(false);
+        mTabSegment.setIndicatorWidthAdjustContent(true);
+        mTabSegment.setDefaultNormalColor(getResources().getColor(R.color.color_0A1D3D));
+        mTabSegment.setDefaultSelectedColor(getResources().getColor(R.color.color_6196FF));
+        mTabSegment.addTab(new QMUITabSegment.Tab(getString(R.string.tabSegment_item_1_title)));
+        mTabSegment.addTab(new QMUITabSegment.Tab(getString(R.string.tabSegment_item_2_title)));
+        mTabSegment.setupWithViewPager(mContentViewPager, false);
+        mTabSegment.setMode(QMUITabSegment.MODE_FIXED);
+        mTabSegment.addOnTabSelectedListener(new QMUITabSegment.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(int index) {
+                mTabSegment.hideSignCountView(index);
+            }
+
+            @Override
+            public void onTabUnselected(int index) {
+
+            }
+
+            @Override
+            public void onTabReselected(int index) {
+                mTabSegment.hideSignCountView(index);
+            }
+
+            @Override
+            public void onDoubleTap(int index) {
+
+            }
+        });
+    }
+
+
 
     @Override
     protected int getLayoutId() {
@@ -47,5 +150,31 @@ public class ConsultFragment extends MvpFragment<ConsultView,ConsultPresenter> i
     public ConsultPresenter createPresenter() {
         getActivityComponent().inject(this);
         return consultPresenter;
+    }
+
+    public enum ContentPage {
+        Item1(0),
+        Item2(1);
+        public static final int SIZE = 2;
+        private final int position;
+
+        ContentPage(int pos) {
+            position = pos;
+        }
+
+        public static ContentPage getPage(int position) {
+            switch (position) {
+                case 0:
+                    return Item1;
+                case 1:
+                    return Item2;
+                default:
+                    return Item1;
+            }
+        }
+
+        public int getPosition() {
+            return position;
+        }
     }
 }
