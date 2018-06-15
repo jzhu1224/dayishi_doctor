d6092ba1a8aa9fe815488a0196a0b6d26c03e6f1
zhujiang <zhujiang.sh@superjia.com>
2018-06-15 18:04:20 +0800

    no message

--- app/src/main/java/com/jkdys/doctor/data/network/DaYiShiServiceApi.java
+++ app/src/main/java/com/jkdys/doctor/data/network/DaYiShiServiceApi.java
@@ -5,11 +5,31 @@ import com.jkdys.doctor.data.model.UserInfoModel;
 
 import java.util.HashMap;
 
+import okhttp3.MultipartBody;
+import okhttp3.RequestBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Multipart;
 import retrofit2.http.POST;
+import retrofit2.http.Part;
 
 public interface DaYiShiServiceApi {
     @POST("/login")
-    public Call<BaseResponse<UserInfoModel>> login(@Body HashMap<String,Object> map);
+    Call<BaseResponse<UserInfoModel>> login(@Body HashMap<String,Object> map);
+
+    /**
+     *
+     *File file = new File(filePath);
+     *RequestBody reqFile = RequestBody.create(MediaType.parse("image/*"), file);
+     *MultipartBody.Part body = MultipartBody.Part.createFormData("upload", file.getName(), reqFile);
+     *RequestBody name = RequestBody.create(MediaType.parse("text/plain"), "upload_test");
+     * @param image
+     * @param name
+     * @return
+     */
+
+    @Multipart
+    @POST("/")
+    Call<ResponseBody> postImage(@Part MultipartBody.Part image, @Part("name") RequestBody name);
 }
