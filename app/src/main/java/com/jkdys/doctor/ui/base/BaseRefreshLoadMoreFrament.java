f4e257e0a8f03349faa9f269e70265bc4afa0881
zhujiang <zhujiang.sh@superjia.com>
2018-07-19 16:49:45 +0800

    empty view

--- app/src/main/java/com/jkdys/doctor/ui/base/BaseRefreshLoadMoreFrament.java
+++ app/src/main/java/com/jkdys/doctor/ui/base/BaseRefreshLoadMoreFrament.java
@@ -16,6 +16,7 @@ import com.chairoad.framework.util.LogUtil;
 import com.jkdys.doctor.R;
 import com.jkdys.doctor.ui.BaseLoadMoreView;
 import com.jkdys.doctor.ui.MvpFragment;
+import com.jkdys.doctor.widget.MyQMUIEmptyView;
 import com.qmuiteam.qmui.widget.QMUIEmptyView;
 import java.util.ArrayList;
 import java.util.List;
@@ -33,7 +34,7 @@ public abstract class BaseRefreshLoadMoreFrament<T extends Parcelable,V extends 
     protected BaseQuickAdapter<T,BaseViewHolder> adapter;
     protected ArrayList<T> mDatas;
 
-    private QMUIEmptyView qmuiEmptyView;
+    private MyQMUIEmptyView qmuiEmptyView;
 
     @Nullable
     @Override
@@ -67,7 +68,7 @@ public abstract class BaseRefreshLoadMoreFrament<T extends Parcelable,V extends 
 
         adapter.setOnItemClickListener(this::onItemClicked);
 
-        qmuiEmptyView = new QMUIEmptyView(getActivity());
+        qmuiEmptyView = new MyQMUIEmptyView(getActivity());
         adapter.setEmptyView(qmuiEmptyView);
 
         if (savedInstanceState != null) {
@@ -156,14 +157,17 @@ public abstract class BaseRefreshLoadMoreFrament<T extends Parcelable,V extends 
     public void showContent() {
         swipeRefreshLayout.setRefreshing(false);
         qmuiEmptyView.show(false);
-        qmuiEmptyView.setTitleText("暂无数据");
+        //qmuiEmptyView.setTitleText("暂无数据");
+        qmuiEmptyView.setDetailText("");
+        qmuiEmptyView.setImageEmpty(R.drawable.img_empty);
     }
 
     @Override
     public void showError(String message) {
         swipeRefreshLayout.setRefreshing(false);
         //showMessage(message);
-        qmuiEmptyView.setTitleText("加载时发生错误");
+        qmuiEmptyView.setImageEmpty(R.drawable.img_load_error);
+       // qmuiEmptyView.setTitleText("");
         qmuiEmptyView.setDetailText(message);
         qmuiEmptyView.setButton("重新加载", view -> loadData(true,true));
     }
